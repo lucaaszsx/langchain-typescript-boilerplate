f1fc29077588a53c18a8d980c9d407312c2f70c3
Lucas <jlucasmelob558@gmail.com>
2026-07-01 16:56:18 -0300

    feat(logger): add more methods

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,23 +1,55 @@
 import { Env } from '../config.js';
 import chalk from 'chalk';
 
-const unshiftInfo = (info: string, messages: unknown[]): unknown[] => {
-    if (messages.length > 0 && typeof messages[0] === 'string')
-        messages[0] = `${info} ${messages[0]}`;
-    else messages.unshift(info);
+enum LogLevel {
+    Log = 'log',
+    Info = 'info',
+    Error = 'error',
+    Debug = 'debug'
+}
 
-    return messages;
+const unshiftInfo = (info: string, messages: unknown[]): unknown[] => {
+    const newMessages = [...messages];
+
+    if (newMessages.length > 0 && typeof newMessages[0] === 'string')
+        newMessages[0] = `${info} ${newMessages[0]}`;
+    else newMessages.unshift(info);
+
+    return newMessages;
 };
 
 export const logger = {
+    logWithTimestamp(level: LogLevel, ...messages: unknown[]): void {
+        const timestamp = chalk.blue(`[${new Date().toISOString()}]`);
+
+        if (level in console) console[level](...unshiftInfo(timestamp, messages));
+        else
+            throw new Error(
+                `unexpected log level "${level}". console object doesn't have that method`
+            );
+    },
+
+    log(...messages: unknown[]): void {
+        console.log(...messages);
+    },
+
+    info(...messages: unknown[]): void {
+        this.logWithTimestamp(LogLevel.Info, ...unshiftInfo(chalk.yellow('[info]'), messages));
+    },
+
+    error(...messages: unknown[]): void {
+        this.logWithTimestamp(LogLevel.Error, ...unshiftInfo(chalk.red('[error]'), messages));
+    },
+
     debug(...messages: unknown[]): void {
         if (!Env.verbose) return;
-
-        const timestamp = chalk.blue(`[${new Date().toISOString()}]`);
-        console.debug(...unshiftInfo(timestamp, messages));
+        this.logWithTimestamp(
+            LogLevel.Debug,
+            ...unshiftInfo(chalk.blueBright('[debug]'), messages)
+        );
     },
 
     debugTool(toolName: string, ...messages: unknown[]): void {
-        this.debug(...unshiftInfo(chalk.yellow(`[tool:${toolName}]`), messages));
+        this.debug(...unshiftInfo(chalk.green(`[tool:${toolName}]`), messages));
     }
 };
